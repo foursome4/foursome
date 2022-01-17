83c6d425734d1ab835318f1da46e2a73eae8139f
Jeferson Macedo <jefersonmacedowgf@gmail.com>
2022-01-17 01:24:42 -0300

    Definindo Package.JSON - Start - Heroku 2

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { Server } from 'socket.io';
 import { router } from "./routes";
 import http from 'http';
+import dotenv from 'dotenv';
+
 
 const app = express();
 const bodyParser = require ('body-parser');
@@ -9,6 +11,8 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const server = http.createServer(app);
 const port = process.env.PORT || 3333;
+
+dotenv.config();
 
 app.use(cors());
 app.use(cookieParser());
