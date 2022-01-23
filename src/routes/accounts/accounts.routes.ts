b16afa0d05f90302b559d4466de1605a01fa5992
Jeferson Macedo <jefersonmacedowgf@gmail.com>
2022-01-23 18:37:47 -0300

    Alterando update informations

--- src/routes/accounts/accounts.routes.ts
+++ src/routes/accounts/accounts.routes.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import { createAccountController } from "../../Modules/Accounts/useCases/createAccount";
 import { listAccountsController } from "../../Modules/Accounts/useCases/listAccounts";
-import { updateInformationsController } from "../../Modules/Accounts/useCases/updateInformations";
 
 import { listGroupController } from "../../Modules/Groups/useCases/listGroup";
 
@@ -13,9 +12,6 @@ groupsAccounts.post("/", (req, res) => {
   return createAccountController.handle(req, res);
 });
 
-groupsAccounts.patch("/", (req, res) => {
-  return updateInformationsController.handle(req, res)
-})
 groupsAccounts.get("/", (req, res) => {
   return listAccountsController.handle(req, res);
 });
