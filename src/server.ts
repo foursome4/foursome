c08fcc6c271e633de303be0b92deda2e7263fc10
Jeferson Macedo <jefersonmacedowgf@gmail.com>
2022-02-01 10:17:19 -0300

    GET Account 3

--- src/server.ts
+++ src/server.ts
@@ -11,9 +11,8 @@ const cors = require('cors');
 const server = http.createServer(app);
 const port = process.env.PORT || 3333;
 
-dotenv.config();
-
 app.use(cors());
+dotenv.config();
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extends: true}));
 app.use(express.json());
