6073973a2078536da64ec312e37d85a193c0b728
Jeferson Macedo <jefersonmacedowgf@gmail.com>
2022-06-06 18:04:59 -0300

    Cors

--- src/http.ts
+++ src/http.ts
@@ -3,11 +3,12 @@ import { Server } from 'socket.io';
 import { router } from "./routes";
 import http from 'http';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 const app = express();
 const bodyParser = require ('body-parser');
 const cookieParser = require('cookie-parser');
-const cors = require('cors');
+
 const server = http.createServer(app);
 
 
@@ -21,7 +22,7 @@ app.use(router);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: ["https://www.foursome.com.br/", "https://adm.foursome.com.br/", "http://localhost:3000/"],
     methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
     credentials: true
   }
