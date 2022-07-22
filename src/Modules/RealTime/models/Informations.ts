2aca9e41a882c2a827e6e1f49fdf09bbe9498acf
Jeferson Macedo <jefersonmacedowgf@gmail.com>
2022-07-22 08:15:21 -0300

    Alterações de Francisco

---
+++ src/Modules/RealTime/models/Informations.ts
@@ -0,0 +1,14 @@
+
+class Informations {
+  id: string;
+  destination: string;
+  idAccount: string;
+  title: string;
+  text: string;
+  link: string;
+  type: string;
+  prioriry: string;
+  created_at: Date;
+}
+
+export { Informations };
