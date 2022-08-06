ec96c32590d19bf2bf2b5963ee500932873e8c06
Jeferson Macedo <jefersonmacedowgf@gmail.com>
2022-08-06 19:37:51 -0300

    Posts

--- src/Modules/Posts/useCases/listPostsAllFilter/ListPostsAllFilterController.ts
+++ src/Modules/Posts/useCases/listPostsAllFilter/ListPostsAllFilterController.ts
@@ -10,14 +10,32 @@ class ListPostsAllFilterController {
 
   async handle(req: Request, res: Response) {
     const typeAccount = req.params;
+    const cityAccount = req.params;
+    const ufAccount = req.params;
     const page = req.query.page;
     const limit = req.query.limit;
 
-   await collections.post.find(typeAccount).sort( { created_at: -1 } ).skip(Number(page) > 0 ? (( Number(page) - 1) * Number(limit)) : 0).limit( Number(limit) ).toArray(function(err, result){
+    const data = req.body
+
+
+
+   await collections.post.find(ufAccount).sort( { created_at: -1 } ).skip(Number(page) > 0 ? (( Number(page) - 1) * Number(limit)) : 0).limit( Number(limit) ).toArray(function(err, result){
       if(err) {
         res.status(500).json(err)
       } else {
-        res.status(200).json(result)
+
+        console.log(data.man)
+        const filterResult = result.filter((results) =>
+              results.typeAccount === data.man ||
+              results.typeAccount === data.woman ||
+              results.typeAccount === data.couple ||
+              results.typeAccount === data.trisal ||
+              results.typeAccount === data.transvestites ||
+              results.typeAccount === data.transsexuals ||
+              results.typeAccount === data.groups
+              )
+             console.log(filterResult)
+        res.status(200).json(filterResult)
       }
      // console.log(result)
       return result;
